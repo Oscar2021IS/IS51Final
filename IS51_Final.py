fda757be53cf9c0d15abcc4b6a0afb13a62571a9
Oscar2021IS <oestrada2021@gmail.com>
2020-12-17 20:42:50 -0800

    structured-english

---
+++ IS51_Final.py
@@ -0,0 +1,17 @@
+"""
+the program is will calculate the class average 
+it will take the values from the txt file
+
+it will take the list of grades
+it will calculate the average  using the average function
+
+it will print "the number of grades"
+the it will print "the average grades"
+
+
+end
+
+
+
+"""
+
