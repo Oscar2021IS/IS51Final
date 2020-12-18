5e681ec0ed02fdde289eb1938dc11135d0afc2fc
Oscar2021IS <oestrada2021@gmail.com>
2020-12-17 20:49:25 -0800

    psuedo-code

--- IS51_Final.py
+++ IS51_Final.py
@@ -10,8 +10,36 @@ the it will print "the average grades"
 
 
 end
+"""
+"""
+def main():
+    file = "Final.txt"
+    calculate_percent_above_average(file)
+
+def calculate_percent_above_average(file):
+    infile = open(file, "r")
+    listGrades = [int(line.rstrip()) for line in infile]
+    infile.close()
+    length = len(listGrades)
+    sum1 = sum(listGrades)
+    avg = sum1 / length
+    print("Number of grades:", length)
+    print("Average grade:", avg)
+    counter = 0
+    for item in listGrades:
+        if item > avg:
+            counter += 1
+    percentHigher = counter / length
+    print("Percent of grades above average:", end =" ")
+    print("{0:.2%}".format(percentHigher))
+
+main()
+
+
+
+
+
 
 
 
 """
-
